5402941ac74234807074afaf490b7a8db75e6906
TURKCELL\EXT02D50984 <murat.sesen@consultant.turkcell.com.tr>
2023-07-30 23:03:58 +0300

    defining test to App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,3 +2,18 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('renders without error', () => {
+  render(<App />);
+})
+
+test('renders the title Live Football World Cup Scoreboard', () => {
+  render(<App />);
+  const title = screen.getByText(/Live Football World Cup Scoreboard/i);
+  expect(title).toBeInTheDocument();
+})
+
+test('Start Match button will be visible on screen', () => {
+  render(<App />);
+  const startMatchButton = screen.getByText(/Start Match/i);
+  expect(startMatchButton).toBeInTheDocument();
+})
