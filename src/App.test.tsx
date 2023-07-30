2e6f616465f7aca84cdbe59a9878870825f5e856
TURKCELL\EXT02D50984 <murat.sesen@consultant.turkcell.com.tr>
2023-07-30 23:21:56 +0300

    App component ready for new match form

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { act } from 'react-dom/test-utils';
 
 test('renders without error', () => {
   render(<App />);
@@ -12,8 +13,19 @@ test('renders the title Live Football World Cup Scoreboard', () => {
   expect(title).toBeInTheDocument();
 })
 
-test('Start Match button will be visible on screen', () => {
+test('renders "Start Match" button when no match is in progress', () => {
   render(<App />);
   const startMatchButton = screen.getByText(/Start Match/i);
   expect(startMatchButton).toBeInTheDocument();
+})
+
+test('opens new match form when "Start Match" button is clicked', async () => {
+  render(<App />);
+  const startMatchButton = screen.getByText(/Start Match/i);
+  act(() => {
+    startMatchButton.click();
+  });
+  const newMatchForm = await screen.findByText(/New Match/i);
+
+  expect(newMatchForm).toBeInTheDocument();
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function App() {
+  const [showNewMatchForm, setShowNewMatchForm] = React.useState(false)
+  const [match, setMatch] = React.useState()
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Live Football World Cup Scoreboard</h1>
+      <button onClick={()=>setShowNewMatchForm(true)}>Start Match</button>
+
+      {showNewMatchForm && <h2>New Match</h2>}
     </div>
   );
 }
