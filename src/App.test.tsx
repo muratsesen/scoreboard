34488b27e59cb6a2b4cdcb91a1c4d63f4d5ac2e3
TURKCELL\EXT02D50984 <murat.sesen@consultant.turkcell.com.tr>
2023-07-31 05:11:28 +0300

    new match form crated with tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react';
+import App, { NewMatchForm } from './App';
 import { act } from 'react-dom/test-utils';
+import { unmountComponentAtNode } from 'react-dom';
 
 describe('App', () => {
   test('renders without error', () => {
@@ -32,100 +33,88 @@ describe('App', () => {
   })
 })
 
-describe('new match form', () => {
-  test('renders "Home Team" input', () => {
-    render(<App />);
-    const startMatchButton = screen.getByText(/Start Match/i);
-    act(() => {
-      startMatchButton.click();
-    });
-    const homeTeamInput = screen.getByLabelText(/Home Team/i);
-    expect(homeTeamInput).toBeInTheDocument();
+describe('New match form', () => {
+  const onCancelMock = jest.fn();
+  const onSubmitMock = jest.fn();
+  describe('layout', () => {
+    beforeEach(() => {
+      render(<NewMatchForm onCancel={onCancelMock} onSubmit={onSubmitMock} />);
+    })
+
+    test('renders "Home Team" input', () => {
+      const homeTeamInput = screen.getByLabelText(/Home Team/i);
+      expect(homeTeamInput).toBeInTheDocument();
+    })
+    
+    test('renders "Away Team" input', () => {
+      const awayTeamInput = screen.getByLabelText(/Away Team/i);
+      expect(awayTeamInput).toBeInTheDocument();
+    })
+    
+    
+    test('renders "Submit" button', () => {
+      const submitButton = screen.getByText(/Submit/i);
+      expect(submitButton).toBeInTheDocument();
+    })
+    
+    test('renders "Cancel" button', () => {
+      const cancelButton = screen.getByText(/Cancel/i);
+      expect(cancelButton).toBeInTheDocument();
+    })
   })
 
-  test('renders "Away Team" input', () => {
-    render(<App />);
-    const startMatchButton = screen.getByText(/Start Match/i);
-    act(() => {
-      startMatchButton.click();
-    });
-    const awayTeamInput = screen.getByLabelText(/Away Team/i);
-    expect(awayTeamInput).toBeInTheDocument();
+  describe('actions', () => {
+   
+    beforeEach(()=>{
+      render(<NewMatchForm onCancel={onCancelMock} onSubmit={onSubmitMock} />);
+    })
+    test('renders Submit button disabled when input fields are empty', () => {
+      const submitButton = screen.getByText(/Submit/i);
+      expect(submitButton).toBeDisabled();
+    })
+
+    test('renders Submit button enabled when input fields are not empty', () => {
+      const homeTeamInput = screen.getByLabelText(/Home Team/i);
+      const awayTeamInput = screen.getByLabelText(/Away Team/i);
+
+      fireEvent.change(homeTeamInput, { target: { value: 'Home Team A' } });
+      fireEvent.change(awayTeamInput, { target: { value: 'Away Team B' } });
+
+      const submitButton = screen.getByText(/Submit/i);
+      expect(submitButton).toBeEnabled();
+    })
+
+    test('submit button works properly', async () => {
+
+      const homeTeamInput = await screen.findByLabelText(/Home Team/i);
+      const awayTeamInput = await screen.findByLabelText(/Away Team/i);
+      const submitButton = await screen.findByText(/Submit/i);
+
+      fireEvent.change(homeTeamInput, { target: { value: 'Home Team A' } });
+      fireEvent.change(awayTeamInput, { target: { value: 'Away Team B' } });
+
+      fireEvent.click(submitButton);
+
+      expect(onSubmitMock).toHaveBeenCalledWith({
+        homeTeam: {
+          name: 'Home Team A',
+          score: 0
+        },
+        awayTeam: {
+          name: 'Away Team B',
+          score: 0
+        },
+        startDateTime: new Date('2011-11-11T12:00:00.000Z')
+      });
+
+    })
+
+    test('cancel button works properly', async () => {
+      const cancelButton = await screen.findByText(/Cancel/i);
+      fireEvent.click(cancelButton);
+      expect(onCancelMock).toHaveBeenCalled();
+  })
   })
 
-  test('renders "Home Team Score" input', () => {
-    render(<App />);
-    const startMatchButton = screen.getByText(/Start Match/i);
-    act(() => {
-      startMatchButton.click();
-    });
-    const homeTeamScoreInput = screen.getByLabelText(/Home Team Score/i);
-    expect(homeTeamScoreInput).toBeInTheDocument();
-    expect(homeTeamScoreInput).toHaveValue(0);
-    expect(homeTeamScoreInput).toHaveAttribute('type', 'number');
-    expect(homeTeamScoreInput).toHaveAttribute('min', '0');
-    expect(homeTeamScoreInput).toHaveAttribute('max', '99');
-    expect(homeTeamScoreInput).toHaveAttribute('step', '1');
-    expect(homeTeamScoreInput).toHaveAttribute('name', 'homeTeamScore');
-    expect(homeTeamScoreInput).toHaveAttribute('id', 'homeTeamScore');
-    expect(homeTeamScoreInput).toHaveAttribute('required');
-  })
 
-  test('renders "Away Team Score" input', () => {
-    render(<App />);
-    const startMatchButton = screen.getByText(/Start Match/i);
-    act(() => {
-      startMatchButton.click();
-    });
-    const awayTeamScoreInput = screen.getByLabelText(/Away Team Score/i);
-    expect(awayTeamScoreInput).toBeInTheDocument();
-    expect(awayTeamScoreInput).toHaveValue(0);
-    expect(awayTeamScoreInput).toHaveAttribute('type', 'number');
-    expect(awayTeamScoreInput).toHaveAttribute('min', '0');
-    expect(awayTeamScoreInput).toHaveAttribute('max', '99');
-    expect(awayTeamScoreInput).toHaveAttribute('step', '1');
-    expect(awayTeamScoreInput).toHaveAttribute('name', 'awayTeamScore');
-    expect(awayTeamScoreInput).toHaveAttribute('id', 'awayTeamScore');
-    expect(awayTeamScoreInput).toHaveAttribute('required');
-  })
-
-  test('renders "Submit" button', () => {
-    render(<App />);
-    const startMatchButton = screen.getByText(/Start Match/i);
-    act(() => {
-      startMatchButton.click();
-    });
-    const submitButton = screen.getByText(/Submit/i);
-    expect(submitButton).toBeInTheDocument();
-  })
-
-  test('renders "Cancel" button', () => {
-    render(<App />);
-    const startMatchButton = screen.getByText(/Start Match/i);
-    act(() => {
-      startMatchButton.click();
-    });
-    const cancelButton = screen.getByText(/Cancel/i);
-    expect(cancelButton).toBeInTheDocument();
-  })
-
-  test('renders "Home Team Score" input with value 0', () => {
-    render(<App />);
-    const startMatchButton = screen.getByText(/Start Match/i);
-    act(() => {
-      startMatchButton.click();
-    });
-    const homeTeamScoreInput = screen.getByLabelText(/Home Team Score/i);
-    expect(homeTeamScoreInput).toHaveValue(0);
-  })
-
-  test('renders "Away Team Score" input with value 0', () => {
-    render(<App />);
-    const startMatchButton = screen.getByText(/Start Match/i);
-    act(() => {
-      startMatchButton.click();
-    });
-    const awayTeamScoreInput = screen.getByLabelText(/Away Team Score/i);
-    expect(awayTeamScoreInput).toHaveValue(0);
-  })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,56 @@
 import React from 'react';
+import { MatchImpl, NewMatchFormProps } from './types';
 
 function App() {
   const [showNewMatchForm, setShowNewMatchForm] = React.useState(false)
   const [match, setMatch] = React.useState()
+
+  function startMatch() {
+   
+  }
   return (
     <div>
       <h1>Live Football World Cup Scoreboard</h1>
-      <button onClick={()=>setShowNewMatchForm(true)}>Start Match</button>
+      <button onClick={() => setShowNewMatchForm(true)}>Start Match</button>
 
-      {showNewMatchForm && <h2>New Match</h2>}
+      {showNewMatchForm && <NewMatchForm onSubmit={startMatch} onCancel={() => setShowNewMatchForm(false)} />}
     </div>
   );
+}
+
+
+export function NewMatchForm({ onSubmit, onCancel }: NewMatchFormProps) {
+  const [homeTeam, setHomeTeam] = React.useState("")
+  const [awayTeam, setAwayTeam] = React.useState("")
+
+  function submitMatch() {
+    const _homeTeam = {
+      name: homeTeam,
+      score: 0
+    };
+    const _awayTeam = {
+      name: awayTeam,
+      score: 0
+    };
+
+    const exampleMatch: MatchImpl = new MatchImpl( _homeTeam, _awayTeam, new Date('2011-11-11T12:00:00.000Z'));
+
+    onSubmit(exampleMatch);
+  }
+  const isSubmitDisabled = homeTeam.length === 0 || awayTeam.length === 0;
+  return (
+    <div>
+      <h2>New Match</h2>
+      <label htmlFor="homeTeam">Home Team:
+        <input value={homeTeam} onChange={(e) => setHomeTeam(e.target.value)} type="text" id="homeTeam" /></label>
+
+      <label htmlFor="awayTeam">Away Team:
+        <input value={awayTeam} onChange={(e) => setAwayTeam(e.target.value)} type="text" id="awayTeam" /></label>
+
+      <button onClick={submitMatch} disabled={isSubmitDisabled}>Submit</button>
+      <button onClick={onCancel}>Cancel</button>
+    </div>
+  )
 }
 
 export default App;
